fe5e4e88227cb3243eb536e2008e9a328e353fa6
asotogarc <103063017+asotogarc@users.noreply.github.com>
2024-09-03 00:10:15 +0200

    Update config.py

--- OfferDataCapture/config.py
+++ OfferDataCapture/config.py
@@ -1,1 +1,15 @@
+import streamlit as st
 
+credentials = {
+  "type": "service_account",
+  "project_id": "shining-env-433615-b9",
+  "private_key_id": "6958f4a5112aee39bc09c1e008967c53bbe7c2f6",
+  "private_key": st.secrets["private_key"],
+  "client_email": "pythonlast@shining-env-433615-b9.iam.gserviceaccount.com",
+  "client_id": "116965150983516086498",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/pythonlast%40shining-env-433615-b9.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
